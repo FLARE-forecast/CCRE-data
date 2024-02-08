6b5806490dc04c9fcbd161c08be66886b0b0531a
Adrienne Breef-Pilz <68744051+abreefpilz@users.noreply.github.com>
2024-02-08 13:22:13 -0500

    trying to look for data package in staging. changed to production

--- R/QAQC_met_workflow.R
+++ R/QAQC_met_workflow.R
@@ -22,7 +22,8 @@ source('R/met_qaqc_function.R')
 ## identify latest date for data on EDI (need to add one (+1) to both dates because we want to exclude all possible start_day data and include all possible data for end_day)
 package_ID <- 'edi.1105.2 ' # published id
 #package_ID <- 'edi.1105.1'
-eml <- read_metadata(package_ID, env = 'staging') ## change once out of staging
+eml <- read_metadata(package_ID) # no longer in staging
+#eml <- read_metadata(package_ID, env = 'staging') ## change once out of staging
 date_attribute <- xml_find_all(eml, xpath = ".//temporalCoverage/rangeOfDates/endDate/calendarDate")
 last_edi_date <- as.Date(xml_text(date_attribute)) + lubridate::days(1)
 
